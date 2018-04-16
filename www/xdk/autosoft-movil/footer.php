df852fe5332d99201822613a4782652e807311de
Linux HP <idvazquezrivera@gmail.com>
2018-04-16 16:06:29 -0500

    50mb exceded

--- www/xdk/autosoft-movil/footer.php
+++
@@ -1,7 +0,0 @@
-<footer id="footer" role="footer">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-sm-12 col-md-12 col-lg-12 text-right"> </div>
-    </div>
-  </div>
-</footer>
