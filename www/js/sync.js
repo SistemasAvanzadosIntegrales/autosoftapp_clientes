244da759df7b9c4d20c69cabb522469372fa1600
pablo_diaz@avansys.com.mx <pablo_diaz@avansys.com.mx>
2018-07-03 15:11:35 -0500

    commit

--- www/js/sync.js
+++ www/js/sync.js
@@ -48,17 +48,17 @@ function __sync_data(data, call_back_function = null){
             var inspection =  data.inspections[i];
 
             var presupuesto = data.presupuestos[inspection.id] ? data.presupuestos[inspection.id] : '';
-            var sql = "INSERT INTO inspections (id, vehicle_id, user_id, origen, status, presupuesto, created_at, updated_at) VALUES ("+inspection.id+", "+inspection.vehicle_id+", "+inspection.user_id+", 'server', "+inspection.status+", '"+ presupuesto +"', '"+inspection.created_at+"', '"+inspection.updated_at+"')";
-            alert(sql);
+            var sql = "INSERT INTO inspections (id, vehicle_id, user_id, origen, status, presupuesto, created_at, updated_at) VALUES ("+inspection.id+", "+inspection.vehicle_id+", "+inspection.user_id+", 'server', "+inspection.status+", '"+ presupuesto +"', '"+inspection.created_at+"', '"+inspection.updated_at+"')";            
             tx.executeSql(sql);
             var vehicle_inspections = inspection.vehicle_inspections;
-            /*for(var x = 0; x < vehicle_inspections.length; x++)
+            for(var x = 0; x < vehicle_inspections.length; x++)
             {
                 var point = vehicle_inspections[x];
                 var files = JSON.stringify(point.files);
                 var sql2 = "INSERT INTO vehicle_inspections (id, inspection_id, point_id, price, severity, status, cataloge, category, origen, files) VALUES ("+point.id+", "+point.inspections_id+", "+point.inspection_id+", '"+point.price+"', "+point.severity+", "+point.status+", '"+point.catalogue.name+"', '"+point.catalogue.inspection.name+"', 'server', '"+files+"' )";
                 tx.executeSql(sql2);
-            }*/
+                alert(sql2);
+            }
         }
     }, function(error) {
         debug('algo fallo', true);
