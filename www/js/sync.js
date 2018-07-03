fed5e40e86f516634a01b15a451223b8f9f56a13
pablo_diaz@avansys.com.mx <pablo_diaz@avansys.com.mx>
2018-07-03 14:01:17 -0500

    commit

--- www/js/sync.js
+++ www/js/sync.js
@@ -47,8 +47,9 @@ function __sync_data(data, call_back_function = null){
         {
             var inspection =  data.inspections[i];
 
-            /*var presupuesto = data.presupuestos[inspection.id] ? data.presupuestos[inspection.id] : '';
+            var presupuesto = data.presupuestos[inspection.id] ? data.presupuestos[inspection.id] : '';
             var sql = "INSERT INTO inspections (id, vehicle_id, user_id, origen, status, presupuesto, created_at, updated_at) VALUES ("+inspection.id+", "+inspection.vehicle_id+", "+inspection.user_id+", 'server', "+inspection.status+", '"+ presupuesto +"', '"+inspection.created_at+"', '"+inspection.updated_at+"')";
+            alert(sql);
             tx.executeSql(sql);
             var vehicle_inspections = inspection.vehicle_inspections;
             /*for(var x = 0; x < vehicle_inspections.length; x++)
